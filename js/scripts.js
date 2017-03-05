0d99631de3d7dd8227df5f7ad194de1340f2d8d6
Susha Dore <susha.dore@gmail.com>
2017-03-04 16:56:31 -0800

    separate business logic and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+var space = " ";
+
 $(function () {
   $("form").submit(function(event) {
     event.preventDefault();
 
     var originalSentence = $("#sentence").val();
-    var space = " ";
     var words = originalSentence.split(space);
     var longWords = words.map(function(word) {
       if (word.length >= 3) {
